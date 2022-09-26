86ff9c53a6bdfb463db4856693d180737c489a72
Polina <polinka29enp@gmail.com>
2022-09-26 20:48:06 +0300

    Zadacha2

---
+++ Zadacha2.py
@@ -0,0 +1,6 @@
+# Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print((not(x or y or z)) == (not x and not y and not z))
+            
